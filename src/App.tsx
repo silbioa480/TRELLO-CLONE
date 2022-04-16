e07360fb08fbd0ffa0074e24910303fa06a67658
silbioa480 <silbioa480@gmail.com>
2022-04-16 23:11:30 +0900

    making dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { toDoState } from "./atoms";
+import DraggableCard from "./components/DraggableCard";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 300px;
+`;
 
 function App() {
-  return <div></div>;
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    setToDos((prev) => {
+      const toDosCopy = [...prev];
+
+      // 1) Delete item on source.index
+      toDosCopy.splice(source.index, 1);
+
+      // 2) Put back the item on the destination.index
+      toDosCopy.splice(destination?.index as number, 0, draggableId);
+
+      return toDosCopy;
+    });
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(provided) => (
+              <Board ref={provided.innerRef} {...provided.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <DraggableCard key={toDo} toDo={toDo} index={index} />
+                ))}
+                {provided.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
 }
 
 export default App;
